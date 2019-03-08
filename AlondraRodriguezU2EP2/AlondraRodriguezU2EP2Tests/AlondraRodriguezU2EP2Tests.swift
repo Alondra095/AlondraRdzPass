30b7d6c7f8bf13081d5c5ea5fbd4dcb5a48a2f58
Alondra095 <robot_095@hotmail.com>
2019-03-08 12:15:02 -0600

    Pass
    Pass

---
+++ AlondraRodriguezU2EP2/AlondraRodriguezU2EP2Tests/AlondraRodriguezU2EP2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  AlondraRodriguezU2EP2Tests.swift
+//  AlondraRodriguezU2EP2Tests
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 19-03-08.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import XCTest
+@testable import AlondraRodriguezU2EP2
+
+class AlondraRodriguezU2EP2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
